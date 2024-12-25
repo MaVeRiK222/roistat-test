bbe8eef5060e167c03a8bbe7e208632eef3ec5e7
Alexey <aleksey_panfilov@bk.ru>
2024-12-25 17:50:17 +0300

    roistat | тестовое | рефакторинг

--- src/js/sendForm.js
+++ src/js/sendForm.js
@@ -6,31 +6,23 @@ let roistatFormHanlderfunction = function () {
         'phone': validatePhone,
         'price': validatePrice,
     }
-    // alert('Подключились');
-    const $form = $('form.form');
-    console.log($form);
-
 
     $(document).on('submit', 'form.form', function (event) {
         event.preventDefault();
         let data = $(this).serializeArray();
-        console.log(data);
         if (isDataValid(data)) {
             sendData(data);
         }
         else { console.log('Данные не прошли валидацию'); }
-        console.log('После обработки');
-        console.log(data);
     });
 
     setTimeout(() => {
         $('form.form input[hidden]').attr({ 'value': 1 });
-    }, 1000 * 10);
+    }, 1000 * 30);
 
     function isDataValid(data) {
         errMessage = "";
         for (let i in data) {
-            console.log(i);
             if (validateFunctions[data[i].name] != undefined) {
                 let validateResponse = validateFunctions[data[i].name](data[i]);
                 if (validateResponse.errMessage.length != 0) errMessage += validateResponse.errMessage + '\n';
@@ -50,6 +42,7 @@ let roistatFormHanlderfunction = function () {
             data: data,
             success: function (response) {
                 alert("Данные отправлены");
+                console.log(response);
             },
             error: function (xhr, status, error) {
                 alert("Произошла ошибка " + error);
@@ -59,61 +52,48 @@ let roistatFormHanlderfunction = function () {
 
     function validateName(data) {
         let errMessage = '';
-        console.log('validateName');
-        console.log(data);
         let flag = true;
         //Предположим что в имени могут быть только буквы и пробел
         let filtredValue = data.value.replace(/[^a-zA-Zа-яА-Я ]/g, '');
-        console.log(filtredValue);
+
         if (filtredValue == 0 || filtredValue.length != data.value.length) {
             flag = false
             errMessage = 'Некорректное имя';
         }
         else { data.value = filtredValue };
-        console.log(data);
         return { isValid: flag, errMessage: errMessage };
     }
 
     function validatePhone(data) {
         let errMessage = '';
-        console.log('validatePhone');
-        console.log(data);
         filtredValue = data.value.replace(/[^0-9+() -]/g, '');
-        console.log(filtredValue);
         // Предположим что номера только российские
         if (filtredValue.length == 0 || filtredValue.length != 11 || filtredValue.length != data.value.length) {
             flag = false;
             errMessage = 'Некорректный номер телефона';
         }
         else data.value = filtredValue;
-        console.log(data);
         return { isValid: flag, errMessage: errMessage };
     }
 
     function validateEmail(data) {
         let errMessage = '';
-        console.log('validateEmail');
         const regex = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
         flag = regex.test(data.value);
         if (!flag) {
             errMessage = 'Некорректная почта';
         }
-        console.log(data);
         return { isValid: flag, errMessage: errMessage };
     }
 
     function validatePrice(data) {
         let errMessage = '';
         let flag = true;
-        console.log('validatePrice');
-        console.log(data);
         let filtredValue = data.value.replace(/[^0-9]/g, '');
-        console.log(filtredValue);
         if (data.value.length != filtredValue.length) {
             flag = false;
             errMessage = 'Некорректная цена';
         }
-        console.log(data);
         return { isValid: flag, errMessage: errMessage };
     }
 }();
